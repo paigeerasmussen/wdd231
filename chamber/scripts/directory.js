b991c7f730a2e6c73bcbba857015fbcfaae8c8c7
paigeerasmussen <paigeerasmussen@gmail.com>
2026-01-16 15:14:09 -0700

    updates

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -11,3 +11,25 @@ navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navBar.classList.toggle('show');
 });
+
+const cards = document.querySelector('#cards');
+
+async function getMembersData() {
+    const response = await fetch('chamber/data/members.json');
+    const data = await response.json();
+    displayMembers(data.members);
+};
+getMembersData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let coName = document.createElement('h2');
+
+        coName.textContent = member.name;
+
+        card.appendChild(coName);
+
+        cards.appendChild(card);
+    });
+};
