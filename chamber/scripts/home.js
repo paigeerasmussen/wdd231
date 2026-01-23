7401f9f17a426b60f79a43c38899cab62cba55a7
paigeerasmussen <paigeerasmussen@gmail.com>
2026-01-23 14:08:53 -0700

    updates

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -17,16 +17,32 @@ const spotlight = document.querySelector('#spotlight');
 async function getMembersData() {
     const response = await fetch('https://paigeerasmussen.github.io/wdd231/chamber/data/members.json');
     const data = await response.json();
-    displaySpotlight(data.members);
+    const randomSpotlights = selectRandomSpotlights(data.members);
+    displaySpotlight(randomSpotlights);
 };
 getMembersData();
 
-// function shuffleSpotlight(members) {
-//     for (let i = members.length - 1: i > 0: i--) {
-//         const j = Math.floor(Math.random() * (i +1));
-//         [members[i], members[j]] = [members[j], members[i]]
-//     }
-// });
+function selectRandomSpotlights(members) {
+
+    const qualifiedMembers = members.filter(
+        member => member.membership === 2 || member.membership === 3
+    );
+
+    if (qualifiedMembers.length < 2) {
+        throw new Error("Array is too short");
+    }
+
+    let index1 = Math.floor(Math.random() * qualifiedMembers.length);
+    let index2 = Math.floor(Math.random() * qualifiedMembers.length);
+    while (index2 === index1) {
+        index2 = Math.floor(Math.random() * qualifiedMembers.length);
+    }
+    let index3 = Math.floor(Math.random() * qualifiedMembers.length);
+    while (index3 === index1 || index3 === index2) {
+        index3 = Math.floor(Math.random() * qualifiedMembers.length);
+    }
+    return [qualifiedMembers[index1], qualifiedMembers[index2], qualifiedMembers[index3]]
+};
 
 const displaySpotlight = (members) => {
     members.forEach((member) => {
@@ -35,12 +51,20 @@ const displaySpotlight = (members) => {
         let coAddress = document.createElement('p');
         let coPhone = document.createElement('p');
         let coWebsite = document.createElement('p');
+        let coStatus = document.createElement('p');
         let coImage = document.createElement('img');
 
         coName.textContent = member.name;
         coAddress.innerHTML = `<strong>ADDRESS:</strong> ${member.address}`;
         coPhone.innerHTML = `<strong>PHONE:</strong> ${member.phone}`;
         coWebsite.innerHTML = `<strong>URL:</strong> ${member.website}`;
+
+        if (member.membership === 2) {
+            coStatus.textContent = "Membership Level: Silver"
+        } else if (member.membership === 3) {
+            coStatus.textContent = "Membership Level: Gold"
+        }
+        coStatus.setAttribute("class", "costatus")
 
         coImage.setAttribute("src", `images/${member.image}`);
         coImage.setAttribute("alt", `${member.name} Company`);
@@ -53,6 +77,7 @@ const displaySpotlight = (members) => {
         spotlightCard.appendChild(coAddress);
         spotlightCard.appendChild(coPhone);
         spotlightCard.appendChild(coWebsite);
+        spotlightCard.appendChild(coStatus);
 
         spotlight.appendChild(spotlightCard);
     });
@@ -75,6 +100,7 @@ const myLong = "-111.85160967993846"
 const myKey = "36d89319046d7f583b18d6bb4cdc23be"
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
 
 async function apiFetch() {
     try {
@@ -83,6 +109,21 @@ async function apiFetch() {
             const data = await response.json();
             console.log(data);
             displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function forecastFetch() {
+    try {
+        const response = await fetch(forecastURL);
+        if (response.ok) {
+            const forData = await response.json();
+            console.log(forData);
+            displayForecast(forData);
         } else {
             throw Error(await response.text());
         }
@@ -110,31 +151,20 @@ function displayResults(data) {
     const sunsetTime = sunsetDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     sunrise.innerHTML = sunriseTime;
     sunset.innerHTML = sunsetTime;
+}
 
-    let date = new Date(data.dt * 1000);
+function displayForecast(forData) {
+    let date = new Date(forData.list[0].dt * 1000);
     let dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
-    const nextDay = new Date();
-    nextDay.setDate(nextDay.getDate() + 1);
+    const nextDay = new Date(forData.list[9].dt * 1000);
     let nextDayOfWeek = nextDay.toLocaleDateString('en-US', { weekday: 'long' });
-    const dayAfter = new Date();
-    dayAfter.setDate(dayAfter.getDate() + 2);
+    const dayAfter = new Date(forData.list[16].dt * 1000);
     let thirdDayOfWeek = dayAfter.toLocaleDateString('en-US', { weekday: 'long' });
 
-    currentDay.innerHTML = `${dayOfWeek}: ${data.main.temp_max}&deg;F`;
-    dayTwo.innerHTML = `${nextDayOfWeek}: ${data.main.temp_max}&deg;F`;
-    dayThree.innerHTML = `${thirdDayOfWeek}: ${data.main.temp_max}&deg;F`;
-
-    // for (let i = 1; i <= 3; i++) {
-    //     const forecastContainer = document.getElementById('forecast')
-    //     const dailyData = data[i];
-    //     const date = new Date(data.dt * 1000);
-    //     const dayName = date.toLocaleDateString("en-US", { weekday: "long" });
-    //     const temp = dailyData.temp.day.toFixed(0);
-
-    //     const dayForcast = document.createElement('div');
-    //     dayForcast.innerHTML = `${dayName}: ${temp}&deg;F`;
-    //     forecastContainer.appendChild(dayForcast);
-    // }
+    currentDay.innerHTML = `${dayOfWeek}: ${forData.list[0].main.temp}&deg;F`;
+    dayTwo.innerHTML = `${nextDayOfWeek}: ${forData.list[9].main.temp}&deg;F`;
+    dayThree.innerHTML = `${thirdDayOfWeek}: ${forData.list[17].main.temp}&deg;F`;
 }
 
 apiFetch()
+forecastFetch()
